777bf8731fe15942dac9c6d73ad7fa45b279f816
LuchinBot <LuchinBot.2018@gmail.com>
2024-09-10 14:55:09 -0500

    Get and Port

--- app.mjs
+++ app.mjs
@@ -11,6 +11,9 @@ app.use(json())
 app.use(corsMiddleware())
 app.disable('x-powered-by')
 
+app.use('/', (req, res) => {
+  res.send('Hello World!')
+})
 app.use('/test', testRouter)
 app.use('/test2', test2Router)
 
@@ -19,7 +22,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!')
 })
 // Port
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT ?? 3001
 
 app.listen(PORT, () => {
   console.log(`Listening on port http://localhost:${PORT}`)
